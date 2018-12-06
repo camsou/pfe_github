6f50915504f96197256bf0c5443cf1d93ca07263
LAPTOP-VPKFPQAM\camil <camille.zordan@hotmail.fr>
2018-12-06 10:03:13 +0100

    Rename NewAccountActivity
    J'ai renommé "activity_nouveau_compte" et "NouveauCompte.java" en anglais comme ça tous les fichiers sont en anglais sinon ca fait bizarre

--- DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
+++ DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
@@ -3,11 +3,11 @@ package com.example.camil.detectnalert;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NouveauCompte extends AppCompatActivity {
+public class NewAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nouveau_compte);
+        setContentView(R.layout.activity_new_account);
     }
 }
