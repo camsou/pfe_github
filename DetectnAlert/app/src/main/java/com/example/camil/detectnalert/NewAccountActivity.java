d0687d8f643fbdfdc41543fc619a6d7c19481ff6
LAPTOP-VPKFPQAM\camil <camille.zordan@hotmail.fr>
2018-12-08 18:33:37 +0100

    Ajoute les liens entre les pages avec le bouton creer nouveau compte
    Attention à bien mettre les lignes super.onCreate et setContentView en
    premier

--- DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
+++ DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
@@ -1,13 +1,29 @@
 package com.example.camil.detectnalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    private Button mCreateNewAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        mCreateNewAccountButton = (Button) findViewById(R.id.CreateNewAccount);
+
+        mCreateNewAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // User clicked the button
+                Intent mainActivityIntent = new Intent(NewAccountActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
     }
 }
