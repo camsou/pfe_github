c4448eb55fc9db84946fa1f610a5264c6489c957
LAPTOP-VPKFPQAM\camil <camille.zordan@hotmail.fr>
2019-01-24 15:19:53 +0100

    Mise à jour de la page création de compte

--- DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
+++ DetectnAlert/app/src/main/java/com/example/camil/detectnalert/NewAccountActivity.java
@@ -6,137 +6,136 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.camil.detectnalert.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewAccountActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = "NewAccountActivity";
 
-    private EditText mEmailView;
-    private EditText mPasswordView;
-    private TextView mStatusTextView;
-    private TextView mDetailTextView;
-
-
-    // [START declare_auth]
+    private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
-    // [END declare_auth]
 
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
 
-        //Button
-        findViewById(R.id.CreateNewAccount).setOnClickListener(this);
-
-        //View
-        mEmailView = findViewById(R.id.Identifiant);
-        mPasswordView = findViewById(R.id.MotDePasse);
-        mStatusTextView = findViewById(R.id.status);
-        mDetailTextView = findViewById(R.id.detail);
-
-        // [START initialize_auth]
-        // Initialize Firebase Auth
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
+
+        // Views
+        mEmailField = findViewById(R.id.Identifiant);
+        mPasswordField = findViewById(R.id.MotDePasse);
+        mSignUpButton = findViewById(R.id.CreateNewAccount);
+
+        // Click listeners
+        mSignUpButton.setOnClickListener(this);
     }
 
-    // [START on_start_check_user]
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-    // [END on_start_check_user]
 
-    private void createAccount(String email, String password) {
-        Log.d(TAG, "createAccount:" + email);
+        // Check auth on Activity start
+        if (mAuth.getCurrentUser() != null) {
+            onAuthSuccess(mAuth.getCurrentUser());
+        }
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
         if (!validateForm()) {
             return;
         }
 
         showProgressDialog();
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
 
-        // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Intent NewAccountIntent = new Intent(NewAccountActivity.this, MainActivity.class);
-                            startActivity(NewAccountIntent);
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(NewAccountActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
                         hideProgressDialog();
-                        // [END_EXCLUDE]
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(NewAccountActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
-        // [END create_user_with_email]
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(NewAccountActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
     }
 
     private boolean validateForm() {
-        boolean valid = true;
-
-        String email = mEmailView.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError("Required.");
-            valid = false;
+        boolean result = true;
+        if (TextUtils.isEmpty(mEmailField.getText().toString())) {
+            mEmailField.setError("Required");
+            result = false;
         } else {
-            mEmailView.setError(null);
+            mEmailField.setError(null);
         }
 
-        String password = mPasswordView.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            mPasswordView.setError("Required.");
-            valid = false;
+        if (TextUtils.isEmpty(mPasswordField.getText().toString())) {
+            mPasswordField.setError("Required");
+            result = false;
         } else {
-            mPasswordView.setError(null);
+            mPasswordField.setError(null);
         }
 
-        return valid;
+        return result;
     }
 
-    private void updateUI(FirebaseUser user) {
-        hideProgressDialog();
-        if (user != null) {
-            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
-                    user.getEmail(), user.isEmailVerified()));
-            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-        } else {
-            mStatusTextView.setText(R.string.signed_out);
-            mDetailTextView.setText(null);
+    // [START basic_write]
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
 
-        }
+        mDatabase.child("users").child(userId).setValue(user);
     }
+    // [END basic_write]
 
     @Override
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.CreateNewAccount) {
-            createAccount(mEmailView.getText().toString(), mPasswordView.getText().toString());
+            signUp();
         }
     }
 }
