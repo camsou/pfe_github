17b6a852d84799295775cc27cd7f7a9416d16360
rymou <taoutirym@gmail.com>
2019-01-25 11:20:01 +0100

    Affichage de la carte info

--- DetectnAlert/app/src/main/java/com/example/camil/detectnalert/Patients.java
+++ DetectnAlert/app/src/main/java/com/example/camil/detectnalert/Patients.java
@@ -2,26 +2,39 @@ package com.example.camil.detectnalert;
 
 public class Patients {
 
-    public int id;
+    public String id;
     public String name;
     public String first_name;
-    public int birth_date;
-    public Character sex;
+    public String timestamp_Ephad;
+    public String sex;
 
     public Patients()
     {
         //Constructeur
     }
 
-    public Patients(int id, String name, String first_name, int birth_date, Character sex){
+    public Patients(String id, String name, String first_name, String timestamp_Ephad, String sex){
         this.id= id;
         this.name = name;
         this.first_name = first_name;
-        this.birth_date = birth_date;
+        this.timestamp_Ephad = timestamp_Ephad;
         this.sex = sex;
     }
     public String GetPatientName() {
         return name;
     }
+    public String GetPatientFirstName() {
+        return first_name;
+    }
+    public String GetPatientTimestamp() {
+        return timestamp_Ephad;
+    }
+    public String GetPatientSex() {
+        return sex;
+    }
+    public String GetPatientID() {
+        return id;
+    }
+
 
 }
--- DetectnAlert/app/src/main/java/com/example/camil/detectnalert/WeightActivity.java
+++ DetectnAlert/app/src/main/java/com/example/camil/detectnalert/WeightActivity.java
@@ -3,8 +3,12 @@ package com.example.camil.detectnalert;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,6 +26,12 @@ public class WeightActivity extends MainActivity  {
     Spinner spinner;
     DatabaseReference db;
     FirebaseHelper helper;
+    TextView id_sex;
+    TextView id_name;
+    TextView id_firstname;
+    TextView id_id;
+    TextView id_timestamp;
+
 
 
     @Override
@@ -35,19 +45,66 @@ public class WeightActivity extends MainActivity  {
 
         db.child("patients").addValueEventListener(new ValueEventListener(){
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
                 final List<String> patients = new ArrayList<String>();
+                final ArrayList<Patients> table = new ArrayList<Patients>();
 
 
                 for (DataSnapshot areaSnapshot: dataSnapshot.getChildren()) {
                     String name = areaSnapshot.child("name").getValue(String.class);
+                    Patients pat = areaSnapshot.getValue(Patients.class);
                     patients.add(name);
+                    table.add(pat);
                 }
 
                 spinner = (Spinner) findViewById(R.id.patient) ;
+                id_sex = (TextView) findViewById(R.id.id_text) ;
+                id_name = (TextView) findViewById(R.id.id_name) ;
+                id_firstname = (TextView) findViewById(R.id.id_firstname) ;
+                id_id = (TextView) findViewById(R.id.id_id) ;
+                id_timestamp = (TextView) findViewById(R.id.id_timestamp) ;
+
+
+
                 ArrayAdapter<String> patients_final = new ArrayAdapter<String>(WeightActivity.this, android.R.layout.simple_spinner_item, patients);
                 patients_final.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(patients_final);
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        String selected = spinner.getItemAtPosition(i).toString();
+
+
+                        String sex;
+                        sex = table.get(i).GetPatientSex();
+                        id_sex.setText(sex);
+
+                        String name;
+                        name = table.get(i).GetPatientName();
+                        id_name.setText(name);
+
+                        String name_first;
+                        name_first = table.get(i).GetPatientFirstName();
+                        id_firstname.setText(name_first);
+
+                        String carte_identite;
+                        carte_identite = table.get(i).GetPatientID();
+                        id_id.setText(carte_identite);
+
+                        String timestamp_ehpad;
+                        timestamp_ehpad = table.get(i).GetPatientTimestamp();
+                        id_timestamp.setText(timestamp_ehpad);
+
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
             }
 
 
